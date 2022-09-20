d050b92c4ca271c6c1e5c356ecc171317506c7fa
milagrosayunta <milagrosaayunta@gmail.com>
2022-09-20 12:35:24 -0300

    PrimerDesafio

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-const objeto_={  
-        name:'jose',
-        edad:27 }
-    const function_ = (nombre)=>{ return console.log("holaa "+nombre)}
-        function_('jose')
-    function_('curso')
+class Usuario{
+    constructor(nombre = "", apellido ="", libros = [], mascotas = []){
+        this.nombre = nombre
+        this.apellido = apellido
+        this.libros = libros
+        this. mascotas = mascotas
+    }
+    getFullName(){
+        return 'El nombre del usuario es: $(this.nombre) $(this.apellido)'
+    }
+    addMascota(mascota){
+        this.mascotas.push(mascota)
+    }
+    addBook(nombre, autor){
+        this.libros.push({nombre, autor})
+    }
+    getBookNames(){
+        return this.libros.map( libro => libro.nombre)
+    }
+}
+
+
+const newUser = new Usuario("Mili", "Ayunta")
+console.log(newUser.getFullName())
+newUser.addBook("Estudio en Rojo", "Arthur Conan Doyle")
+console.log(newUser.getBookNames())
